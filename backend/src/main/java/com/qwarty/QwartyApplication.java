34133de00a2cbc8afdafb28d3f5d1ad4f6d4c8ae
Brill <brilljulianrina@gmail.com>
2025-12-12 15:09:22 -0800

    Added Spotless for linting/formatting backend, Prettier for formatting frontend

--- backend/src/main/java/com/qwarty/QwartyApplication.java
+++ backend/src/main/java/com/qwarty/QwartyApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QwartyApplication {
+public class QwartyApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(QwartyApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(QwartyApplication.class,args);
+    }
 
 }
--- backend/src/test/java/com/qwarty/QwartyApplicationTests.java
+++ backend/src/test/java/com/qwarty/QwartyApplicationTests.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class QwartyApplicationTests {
+class QwartyApplicationTests
+{
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads()
+    {
+    }
 
 }
