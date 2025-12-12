60c82b400b30045e79c8591df12d36fa5d11f192
Brill <brilljulianrina@gmail.com>
2025-12-12 01:01:49 -0800

    Initial commit

---
+++ backend/src/main/java/com/qwarty/QwartyApplication.java
@@ -0,0 +1,13 @@
+package com.qwarty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QwartyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/qwarty/QwartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwarty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QwartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
