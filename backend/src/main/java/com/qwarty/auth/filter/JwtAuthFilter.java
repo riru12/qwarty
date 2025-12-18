5a77a21418a86f737b1b295c98f335dfc717bdb8
Brill <brilljulianrina@gmail.com>
2025-12-17 20:01:48 -0800

    Added @Override to jwt filter method

--- backend/src/main/java/com/qwarty/auth/filter/JwtAuthFilter.java
+++ backend/src/main/java/com/qwarty/auth/filter/JwtAuthFilter.java
@@ -30,6 +30,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final HandlerExceptionResolver handlerExceptionResolver;
 
+    @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
