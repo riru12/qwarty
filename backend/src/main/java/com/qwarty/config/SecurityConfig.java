9f583e70879b8d151ebc13603f2af0d21823f249
Brill <brilljulianrina@gmail.com>
2025-12-17 20:35:33 -0800

    Added authenticationProvider wiring

--- backend/src/main/java/com/qwarty/config/SecurityConfig.java
+++ backend/src/main/java/com/qwarty/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.qwarty.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,19 +17,21 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .sessionManagement(
-                    session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**")
                 .permitAll() // allow login/signup endpoints
                 .anyRequest()
                 .authenticated() // all other requests require authentication
             )
+            .sessionManagement(
+                    session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authenticationProvider(authenticationProvider)
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
