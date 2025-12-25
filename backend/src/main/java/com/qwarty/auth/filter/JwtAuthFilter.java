5be95d5a27c2ded7b05957bdfe09513ab1b5b143
Brill Riña <brilljulianrina@gmail.com>
2025-12-25 14:36:27 -0800

    Added public endpoint JWT filter bypass check (#5)
    * Added public endpoint JWT filter bypass check
    
    * Spotless apply

--- backend/src/main/java/com/qwarty/auth/filter/JwtAuthFilter.java
+++ backend/src/main/java/com/qwarty/auth/filter/JwtAuthFilter.java
@@ -35,7 +35,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
         final String authHeader = request.getHeader("Authorization");
 
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ") || skipJwtFilter(request.getRequestURI())) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -59,5 +59,9 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         } catch (Exception e) {
             handlerExceptionResolver.resolveException(request, response, null, e);
         }
+    }
+
+    private boolean skipJwtFilter(String path) {
+        return path.startsWith("/api/auth/");
     }
 }
