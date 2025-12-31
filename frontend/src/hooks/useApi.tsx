7e42d553bbbf4cf69720d48cdc9860cc9e91e060
Brill <brilljulianrina@gmail.com>
2025-12-31 15:05:29 -0800

    Adjusted useApi hook

--- frontend/src/hooks/useApi.tsx
+++ frontend/src/hooks/useApi.tsx
@@ -8,13 +8,13 @@ import type {
 
 // hook to use api service
 export const useApi = () => {
-    const { accessToken } = useAuth(); // uses hook for auth context to retrieve and use accessToken in requests
+    const auth = useAuth(); // uses hook for auth context to retrieve and use accessToken in requests
 
     const call = async <E extends Endpoint<any, any>>(
         endpoint: E,
         payload?: EndpointReq<E>
     ): Promise<EndpointRes<E>> => {
-        const response = await apiService.call(endpoint, payload, accessToken);
+        const response = await apiService.call(endpoint, payload, auth.accessToken);
 
         return response;
     };
