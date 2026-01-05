d99794cf1eb1e19470e1d5790b6b5c3365c559f9
Brill <brilljulianrina@gmail.com>
2026-01-05 02:55:47 -0800

    Added ModeButton hover sound

--- frontend/src/components/ui/ModeButton.tsx
+++ frontend/src/components/ui/ModeButton.tsx
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+import hoverSoundFile from "@assets/audio/modebuttonhover.wav";
 import "../styles/ModeButton.css";
 
 type ModeButtonProps = {
@@ -6,10 +8,24 @@ type ModeButtonProps = {
     onClick?: () => void;
 };
 
+let lastSoundPlay = 0;
+
 export const ModeButton = ({ name, description, onClick }: ModeButtonProps) => {
+    const hoverSound = useRef(new Audio(hoverSoundFile));
+    hoverSound.current.volume = 0.025;
+    const playSound = () => {
+        const now = Date.now();
+
+        if (now - lastSoundPlay > 100) {
+            lastSoundPlay = now;
+            hoverSound.current.currentTime = 0;
+            hoverSound.current.play().catch(() => {});
+        }
+    };
+
     return (
         <div className="mode-button-container">
-            <div className="mode-button" onClick={onClick}>
+            <div className="mode-button" onClick={onClick} onMouseEnter={playSound}>
                 <div className="mode-details">
                     <h1 className="mode-title">{name}</h1>
                     <p className="mode-description">{description}</p>
