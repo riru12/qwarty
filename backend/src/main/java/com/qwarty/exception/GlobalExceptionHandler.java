12a71a9587da071b15a13094afd841515ce2cf0e
Brill <brilljulianrina@gmail.com>
2026-01-12 13:56:23 -0800

    Spotless apply

--- backend/src/main/java/com/qwarty/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/qwarty/exception/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import java.util.Locale;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
